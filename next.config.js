4e46a218bce3a7c3a0eeff62e7d11a022aad067a
Ashish Malaviya <ashishmalaviya2424@gmail.com>
2023-07-01 10:38:41 +0530

    change configu

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: [ "mongoose" ]
-  }
+  },
+  output: "standalone",
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: [ "default", "en", "nl", "fr" ],
+    defaultLocale: "default",
+    localeDetection: false,
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
