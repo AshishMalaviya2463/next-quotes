3d906a87731e79ebe1f7bc0ecbbaa888536bb71f
Ashish Malaviya <ashishmalaviya2424@gmail.com>
2023-07-01 11:54:21 +0530

    change next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,20 @@ const nextConfig = {
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: [ "mongoose" ]
-  }
+  },
+  async headers () {
+    return [
+      {
+        source: '/api/quotes/all',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
