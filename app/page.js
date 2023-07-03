27d8d95e8a8248f853c72da12964c000a63fb5a8
Ashish Malaviya <ashishmalaviya2424@gmail.com>
2023-07-03 09:24:36 +0530

    rebuild homepage

--- app/page.js
+++ app/page.js
@@ -1,33 +1,35 @@
-import AllQuotesHome from '@/components/allQuotesHome/AllQuotesHome';
 import QuoteListItem from '@/components/quoteListItem/QuoteListItem'
 import { cookies, headers } from 'next/headers';
 import React from 'react'
 
-// const getUser = async () => {
-//   const headersData = headers();
-//   const protocol = headersData.get( "x-forwarded-proto" );
-//   const host = headersData.get( "host" );
-//   const res = await fetch( `${protocol}://${host}/api/quotes/all`, {
-//     cache: "no-cache"
-//   } )
-//   const allQuotes = await res.json()
+const getUser = async () => {
+  const headersData = headers();
+  const protocol = headersData.get( "x-forwarded-proto" );
+  const host = headersData.get( "host" );
+  const res = await fetch( `${protocol}://${host}/api/quotes/all`, {
+    cache: "no-cache"
+  } )
+  const allQuotes = await res.json()
 
-//   return allQuotes
-// }
+  return allQuotes
+}
 
-const Home = () => {
+const Home = async () => {
 
-  // const allQuotes = await getUser()
+  const allQuotes = await getUser()
   const cookieStore = cookies()
   const userData = cookieStore.get( "loginUserData" )
   const coockieUserData = userData === undefined ? undefined : JSON.parse( userData?.value )
 
   return (
     <div className={`container mt-4`}>
-
-      <AllQuotesHome
-        // quotes={allQuotes?.quotes}
-        coockieUserData={coockieUserData} />
+      {
+        allQuotes?.quotes?.length > 0
+          ?
+          allQuotes?.quotes?.map( quote => ( <QuoteListItem key={quote._id} quote={quote} authorName={quote.author_name} coockieUserData={coockieUserData} /> ) )
+          :
+          <h2 className={`text-center text-secondary mt-5 pt-5`}>No Quotes Available.</h2>
+      }
     </div>
   )
 }
